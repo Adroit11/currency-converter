fa972adbc8835199cb822f4165d6af27c1bb1358
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2018-06-26 09:30:47 +0100

    consumed the currencyconverterapi to display all currencies and symbols

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+// Create HTML element
+function createElement(element){
+	return document.createElement(element);
+}
+
+// Set Element's class/ID attributes
+
+function setAttributes(element, name, value){
+	return element.setAttribute(name, value);
+}
+
+// Append to the dropdown
+function appendToParent(parent, child){
+	return parent.appendChild(child);
+}
+
+function getAllCurrency(){
+	const select = document.getElementById('currencies');
+	const url = 'https://free.currencyconverterapi.com/api/v5/currencies';
+	fetch(url)
+	.then(response => response.json())
+	.then(function(data){
+		let currencies = data.results;
+		for(currency in currencies){
+			//console.log(currencies[currency]);
+			let currencyNames = currencies[currency];
+			let option = createElement('option');
+				option.value = `${currencyNames.id}`;
+				option.text = `${currencyNames.currencyName} (${currencyNames.currencySymbol})`;
+			appendToParent(select, option);
+		}
+	})
+	.catch(function(err){
+		console.log(JSON.stringify(err));
+	});
+}
+
+function convertCurrency(amount, fromCurrency, toCurrency){
+	const query = fromCurrency + '_' + toCurrency;
+	const url = 'https://free.currencyconverterapi.com/api/v5/convert?q='+query+'&compact';
+	fromCurrency = encodeURIComponent(fromCurrency);
+	toCurrency = encodeURIComponent(toCurrency);
+	fetch(url)
+	.then(function(response){
+		console.log(response);
+		return response.json();
+	})
+	.then(function(data){
+		console.log(data);
+	})
+}
+getAllCurrency();
+//convertCurrency(10, 'USD', 'NGN');  
+
