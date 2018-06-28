4055f394e66fda6d92667332b2ccb5107c086cb7
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2018-06-29 00:25:04 +0100

    caching the API endpoint

--- serviceworker.js
+++ serviceworker.js
@@ -1,27 +1,12 @@
-let appName = 'currency-converter'     
-let version = 'v1'            
-let cacheName = appName + version
-
-self.addEventListener('fetch', function (event) {
-  console.log('fetch request : ' + event.request.url)
-  event.respondWith(
-    caches.match(event.request).then(function (request) {
-      if (request) { 
-        console.log('responding with cache : ' + event.request.url)
-        return request
-      } else {       
-        console.log('file is not cached, fetching : ' + event.request.url)
-        return fetch(event.request)
-      }
-    })
-  )
-})
+const appName = 'currency-converter'     
+const version = 'v1'            
+const cacheName = appName + version
 
 // Cache resources
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      console.log('installing cache : ' + cacheName)
+      console.log('installing cache app shell : ' + cacheName)
       return cache.addAll([
           '/',
           '/index.html',
@@ -37,18 +22,35 @@ self.addEventListener('install', function (event) {
 self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(appName)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(cacheName)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('deleting cache : ' + key);
+          return caches.delete(key);
         }
       }))
     })
   )
 })
+
+self.addEventListener('fetch', event => {
+  const url = 'https://free.currencyconverterapi.com/api/v5/currencies';
+
+  // If contacting API, fetch and then cache the new data
+  if (event.request.url.indexOf(url) === 0) {
+    event.respondWith(
+      fetch(event.request).then(response =>
+        caches.open(cacheName).then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        }),
+      ),
+    );
+  } else {
+    // Respond with cached content if they are matched
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => response || fetch(event.request)),
+    );
+  }
+});
